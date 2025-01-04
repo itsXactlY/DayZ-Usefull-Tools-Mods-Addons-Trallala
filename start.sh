65ea45104d235ba552261062485e911f201e383a
aLca <6356217+itsXactlY@users.noreply.github.com>
2025-01-04 09:07:45 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 ./start_workshopupdate.sh
 
-sleep 3
+# Define variables
+SERVER_BINARY="./DayZServer"
+CONFIG_FILE="serverDZ_debug.cfg"
+PROFILES_FOLDER="./profile"
+MODS="@CF;@Dabs Framework;@BaseBuildingPlus;@BodyBags;@Breachingcharge;@CarCover;@CJ187-LootChest;@Code Lock;@Movement;@MuchFramework;@MuchStuffPack;@RevGuns;@VPPAdminTools;@Trader;@Paragon Gear and Armor;@Care Packages V2"
 
-./DayZServer -config=serverDZ.cfg -profiles=./profile -logs -mod="@CF;@Dabs Framework;@DayZ-Expansion-Core;@DayZ-Expansion-Chat;@DayZ-Expansion-Hardline;@DayZ-Expansion-Licensed;@DayZ-Expansion-Market;@DayZ-Expansion-Name-Tags;@Movement;@VPPAdminTools"
+# Server startup options
+OPTIONS="-config=$CONFIG_FILE -profiles=$PROFILES_FOLDER -logs -adminlog -mod=\"$MODS\" -nosound -noPause"
+
+# Use Konsole to open a new window and start the server with Fish shell
+konsole --noclose -e fish -c "$SERVER_BINARY $OPTIONS"
