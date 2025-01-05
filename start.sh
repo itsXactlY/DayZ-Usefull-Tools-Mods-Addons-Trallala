7ece891e0b41ea294da5b4085e7932d039a66e98
aLca <6356217+itsXactlY@users.noreply.github.com>
2025-01-05 19:55:09 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,13 +3,12 @@
 
 # Define variables
 SERVER_BINARY="./DayZServer"
-CONFIG_FILE="serverDZ_debug.cfg"
+CONFIG_FILE="serverDZ.cfg"
 PROFILES_FOLDER="./profile"
-MODS="@CF;@Dabs Framework;@BaseBuildingPlus;@BodyBags;@Breachingcharge;@CarCover;@CJ187-LootChest;@Code Lock;@Movement;@MuchFramework;@MuchStuffPack;@RevGuns;@VPPAdminTools;@Trader;@Paragon Gear and Armor;@SNAFU_Weapons;@Care Packages V2"
+MODS="@CF;@Dabs Framework;@BaseBuildingPlus;@BodyBags;@Breachingcharge;@CarCover;@CJ187-LootChest;@Code Lock;@Movement;@MuchFramework;@MuchStuffPack;@RevGuns;@VPPAdminTools;@Trader;@Traveling Trader;@Paragon Gear and Armor;@SNAFU_Weapons;@Care Packages V2"
 
 # Server startup options
-OPTIONS="-config=$CONFIG_FILE -profiles=$PROFILES_FOLDER -logs -adminlog -mod=\"$MODS\" -nosound -noPause"
+OPTIONS="-config=$CONFIG_FILE -profiles=$PROFILES_FOLDER -mod=\"$MODS\" -nosound -noPause -cpuCount=8 -maxMem=16132"
 
 # Use Konsole to open a new window and start the server with Fish shell
 konsole --noclose -e fish -c "$SERVER_BINARY $OPTIONS"
-
